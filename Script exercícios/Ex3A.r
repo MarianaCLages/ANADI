8923583234a5e4c5c7e8dac03209f2a0b8839467
Francisco Redol <1201239@isep.ipp.pt>
2023-04-13 21:45:59 +0100

    Relatório adicionado, com introduções

--- Script exercícios/Ex3A.r
+++ Script exercícios/Ex3A.r
@@ -2,7 +2,7 @@
 
 # Primeiro efetua-se a definicao do caminho em que se encontra o nosso data set, 
 # que contém os dados de 99 viaturas escolhidas aleatoriamente
-setwd("C:/Users/franc/Desktop/Pratico1/")
+setwd("C:/Users/franc/OneDrive - Instituto Superior de Engenharia do Porto/Desktop/3 ANO/ANADI/Pratico1/")
 
 # Importação do ficheiro que contém os dados
 dados <- read.csv("DADOS3.csv",)
@@ -49,7 +49,7 @@ boxplot(dadosAgrupados,
         names= numCilindros,main = 'Aceleração por cada grupo de cilindros', xlab = 'Cilindros', ylab = 'Aceleração')
 
 # Após analisar o boxplot, foi possível confirmar que, de facto, existem outliers,
-# contudo, será feita também a verificação através do R
+# contudo, será feita também a verificação através do cálculo dos valores dos limites inferiores e superiores
 #
 # Para tal, será necessário averiguar se existem valores fora da amplitude dos bigodes.
 # Consequentemente, estes valores terão de ser calculados
@@ -83,3 +83,63 @@ shapiro.test(dados8cyl)
 # o nível de significância é superior a 0,05. Contudo, para os motores de 6 cilindros é menor que 0,05,
 # o que leva a crer que a distrbuição dos dados de aceleração para motores de 6 cilindros não é
 # normal/homogéna
+#
+#
+# Assim, podemos concluir que como uma ou mais hipóteses falham, será necessário recorrer
+# ao uso de testes não paramétricos
+
+
+
+# Para conseguirmos então verificar qual das hipóteses se encontra correta, será necessário
+# usar um teste de Kruskal-Wallis
+
+kruskal.test(Acceleration ~ Cylinders, data = dados)
+
+# Observando que p-value é 2.795e-11, inferior a 0.05, rejeitamos a hipótese h0 e 
+# concluímos que existem diferenças significativas entre os grupos de cilindros
+# 
+#
+
+
+
+
+
+
+
+
+
+weight <- dados$Weight
+horsepower <- dados$Horsepower
+acceleration <- dados$Acceleration
+cylinders <- factor(dados$Cylinders)
+
+
+# criar a variável dummy para o número de cilindros
+
+# ajustar o modelo de regressão linear
+modelo <- lm(Acceleration ~ weight + horsepower + cylinders, data = dados)
+
+# visualizar os resultados
+#plot(modelo)
+
+
+
+
+
+
+
+# criar um data frame com as variáveis independentes para um carro com 6 cilindros, peso 3000 e potência 150
+novos_dados <- data.frame(
+  cylinders = factor(4),
+  weight = 2950,
+  horsepower = 100
+)
+
+# estimar a aceleração usando o modelo de regressão linear
+predicao <- predict(modelo, newdata = novos_dados)
+
+# visualizar a estimativa da aceleração
+predicao
+
+
+
