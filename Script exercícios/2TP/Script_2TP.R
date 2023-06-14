93a6ae6d4c8f2a5ea2f93eecaf7cf6db292e01b9
Miguel Jordão <1201487@isep.ipp.pt>
2023-06-15 00:28:53 +0100

    4.2 : Exercício 3 (NW-> alínea c) + modelo de NN na alínea a))

--- Script exercícios/2TP/Script_2TP.R
+++ Script exercícios/2TP/Script_2TP.R
@@ -12,11 +12,13 @@ library(dplyr)
 library(rpart)
 library(rpart.plot)
 library(neuralnet)
+library(class)
 
 # 1.
 # Definição do caminho em que se encontra o script
 #setwd("C:/Users/franc/Documents/Repositórios/anadi23/Script exercícios/2TP")
-setwd("C:/Users/maria/Desktop/ISEP/3ºano/2ºsemestre/ANADI/anadi23/Script exercícios/2TP")
+#setwd("C:/Users/maria/Desktop/ISEP/3ºano/2ºsemestre/ANADI/anadi23/Script exercícios/2TP")
+setwd("C:/Users/MiguelJordão(1201487/Desktop/ANADI/anadi23/Script exercícios/2TP")
 
 #"/Users/fredol/Documents/isep/anadi23/Script exercícios/2TP"
 #"C:/Users/franc/Documents/Repositórios/anadi23/Script exercícios/2TP"
@@ -361,4 +363,298 @@ t.test(mlr.sample, nn.sample)
 # Como p = 0.9324 > alfa = 0.05, não existe evidência estatística suficiente para se rejeitar H0.
 # Logo, conclui-se que os resultados obtidos para os dois modelos são estatisticamente significativos.
 # O mais eficiente é aquele que apresenta um MAE e um RMSE menor, ou seja, o modelo da regressão linear múltipla.
+
+# Classificação
+
+# 3
+
+sample <- sample(c(TRUE, FALSE), nrow(dataset), replace = TRUE, prob = c(0.70, 0.30))
+
+dataset.train <- dataset[sample,]
+dataset.test <- dataset[!sample,]
+
+#1 internal node
+numnodes <- 1
+
+nn.model.ex.3 <- 
+  neuralnet(
+    gender ~ .,
+    data = dataset.train,
+    hidden = numnodes
+  )
+plot(nn.model.ex.3)
+
+nn.model.ex.3$result.matrix
+
+dif = dataset.test$gender - (
+    dataset.test$Pro.level +
+    dataset.test$Winter.Training.Camp +
+    dataset.test$altitude_results +
+    dataset.test$vo2_results +
+    dataset.test$hr_results +
+    dataset.test$age
+) ; dif
+
+#MAE
+nn.model.ex.3.mae = mean(abs(dif))
+cat("mae : ", nn.model.ex.3.mae)
+
+#mae :  3.096815
+
+#RMSE
+
+nn.model.ex.3.rmse = sqrt(mean(dif^2))
+cat("rmse : ", nn.model.ex.3.rmse)
+
+#rmse : 3.269947
+
+#Knn
+
+sample <- sample(c(TRUE, FALSE), nrow(dataset), replace = TRUE, prob = c(0.70, 0.30))
+
+dataset.train <- dataset[sample,]
+dataset.test <- dataset[-sample,]
+
+train_gender <- dataset[sample, "gender"]
+test_gender <- dataset[-sample, "gender"]
+
+pr <- knn(dataset.train,dataset.test,cl=train_gender,k=5); pr
+tab <- table(pr, test_gender)
+
+accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
+accuracy(tab)
+
+# [1] 100
+
+predictions = factor(pr, levels=levels(dataset.train$gender))
+confusionMatrix(as.factor(predictions), dataset.train$gender)
+
+#### 2nd way ####
+
+data.train <- dataset[sample,]
+data.tst <- dataset[-sample,]
+
+train_labels <- dataset[sample, "gender"]
+tst_labels <- dataset[-sample, "gender"]
+
+k <- c()
+accuracy <- c()
+for (i in seq(1, 50, 2)){   
+  
+  knn.pred <- knn(train=data.train, test=data.tst, cl=train_labels, k=i) 
+  
+  cfmatrix <- table(tst_labels,knn.pred)
+  
+  accuracy <- c(accuracy, sum(diag(cfmatrix))/sum(cfmatrix))
+  
+  k <- c(k,i)
+}
+resNeigh<-data.frame(k,accuracy)
+
+#Max accuracy
+resNeigh[resNeigh$accuracy==max(resNeigh$accuracy), ]   
+plot(resNeigh$k,resNeigh$accuracy)
+
+#Plot Max accuracy
+resNeigh[resNeigh$accuracy == max(resNeigh$accuracy), ]
+k[which.max(accuracy)]
+plot(
+  resNeigh$k,
+  resNeigh$accuracy,
+  col = ifelse(
+    resNeigh$accuracy == max(resNeigh$accuracy),
+    'orangered1',
+    'steelblue4'
+  )
+)
+
+# 1 Todos os k deram accuracy alta... usar k=25 for example
+
+library(rpart)
+
+cvf <- 10
+folds <- sample(1:cvf, nrow(dataset), replace = TRUE)
+
+# Fold size
+table(folds)
+
+accuracy <- matrix(nrow = cvf, ncol = 2)
+k <- 25
+numnodes <- 1
+
+for (i in 1:cvf) {
+  train.cv <- dataset[folds != i, ]
+  test.cv <- dataset[folds == i, ]
+  
+  train_labels <- dataset[folds != i, "gender"]
+  tst_labels <- dataset[folds == i, "gender"]
+  
+  knn.pred <- knn(train = train.cv[, -sample], test = test.cv[, -sample], cl = train_labels, k)
+  cfmatknn <- table(tst_labels, knn.pred)
+  
+  neuralnet.model <- neuralnet(gender ~ ., data = train.cv, hidden = numnodes)
+  neuralnet.pred <- compute(neuralnet.model, test.cv[, -sample])$net.result
+  neuralnet.pred <- ifelse(neuralnet.pred > 0.5, 1, 0)  # Convert probabilities to binary predictions
+  cfmatneuralnet <- table(tst_labels, neuralnet.pred)
+
+  accuracy[i, ] <- c(
+    sum(diag(cfmatknn))/sum(cfmatknn),
+    sum(diag(cfmatneuralnet))/sum(cfmatneuralnet)
+  )
+}
+
+accuracy
+
+apply(accuracy, 2, mean)
+
+#Mean
+# [1] 0.7540967 0.8167644
+
+apply(accuracy, 2, sd)
+
+#Sd
+# [1] 0.04230348 0.12573381
+
+######
+# knn : mean= 0.7540967 & sd = 0.04230348
+# nn : mean= 0.8167644 & sd = 0.12573381
+#####
+
+#b
+
+knn.mean <- apply(accuracy, 2, mean)[1];
+knn.sd <- apply(accuracy, 2, sd)[1];
+
+nn.mean <- apply(accuracy, 2, mean)[2];
+nn.sd <- apply(accuracy, 2, sd)[2];
+
+# 2 melhores modelos: KNN e NN (1 nó interno)
+
+# Criar amostra para cada modelo
+knn.sample <- c(knn.mean, knn.sd)
+nn.sample <- c(nn.mean, nn.sd)
+
+# H0: Os resultados obtidos para os dois modelos são estatisticamente significativos
+# H1: Os resultados obtidos para os dois modelos não são estatisticamente significativos
+
+# Teste t com significância de 5%
+t.test(knn.sample, nn.sample)
+
+# Como p = 0.8964 > alfa = 0.05, não existe evidência estatística suficiente para se rejeitar H0.
+# Logo, conclui-se que os resultados obtidos para os dois modelos são estatisticamente significativos.
+
+# O mais eficiente é aquele que apresenta um MEAN e um SD menor, ou seja, o modelo da rede neuronal.
+
+
+# 3
+
+cvf <- 10
+folds <- sample(1:cvf, nrow(dataset), replace = TRUE)
+
+# Fold size
+table(folds)
+
+accuracy <- matrix(nrow = cvf, ncol = 2)
+sensitivity <- matrix(nrow = cvf, ncol = 2)
+specificity <- matrix(nrow = cvf, ncol = 2)
+f1 <- matrix(nrow = cvf, ncol = 2)
+k <- 25
+numnodes <- 1
+
+for (i in 1:cvf) {
+  train.cv <- dataset[folds != i, ]
+  test.cv <- dataset[folds == i, ]
+  
+  train_labels <- dataset[folds != i, "gender"]
+  tst_labels <- dataset[folds == i, "gender"]
+  
+  knn.pred <- knn(train = train.cv[, -sample], test = test.cv[, -sample], cl = train_labels, k)
+  cfmatknn <- table(tst_labels, knn.pred)
+  
+  neuralnet.model <- neuralnet(gender ~ ., data = train.cv, hidden = numnodes)
+  neuralnet.pred <- compute(neuralnet.model, test.cv[, -sample])$net.result
+  neuralnet.pred <- ifelse(neuralnet.pred > 0.5, 1, 0)  # Convert probabilities to binary predictions
+  cfmatneuralnet <- table(tst_labels, neuralnet.pred)
+  
+  accuracy[i, ] <- c(
+    sum(diag(cfmatknn))/sum(cfmatknn),
+    sum(diag(cfmatneuralnet))/sum(cfmatneuralnet)
+  )
+  
+  # Handle NA values in sensitivity
+  sensitivity_knn <- sensitivity(cfmatknn)[2]
+  sensitivity_neuralnet <- sensitivity(cfmatneuralnet)[2]
+  
+  if (is.na(sensitivity_knn)) sensitivity_knn <- 0
+  if (is.na(sensitivity_neuralnet)) sensitivity_neuralnet <- 0
+  
+  sensitivity[i, ] <- c(
+    sensitivity_knn,
+    sensitivity_neuralnet
+  )
+  
+  specificity[i, ] <- c(
+    specificity(cfmatknn)[1],
+    specificity(cfmatneuralnet)[1]
+  )
+  
+  f1[i, ] <- c(
+    (2 * cfmatknn[2, 2]) / (sum(cfmatknn[, 2]) + sum(cfmatknn[2, ])),
+    (2 * cfmatneuralnet[2, 2]) / (sum(cfmatneuralnet[, 2]) + sum(cfmatneuralnet[2, ]))
+  )
+  
+}
+
+# Calculate the average performance metrics across folds
+mean_accuracy <- apply(accuracy, 2, mean)
+mean_sensitivity <- apply(sensitivity, 2, mean)
+mean_specificity <- apply(specificity, 2, mean)
+mean_f1 <- apply(f1, 2, mean)
+
+# Print the average performance metrics
+cat("Mean Accuracy:", mean_accuracy, "\n")
+cat("Mean Sensitivity:", mean_sensitivity, "\n")
+cat("Mean Specificity:", mean_specificity, "\n")
+cat("Mean F1:", mean_f1, "\n")
+
+
+##########
+
+
+
+# Calculate performance metrics for KNN model
+knn_accuracy <- sum(diag(cfmatknn)) / sum(cfmatknn)
+knn_sensitivity <- sensitivity(cfmatknn)
+knn_specificity <- specificity(cfmatknn)
+knn_f1 <- F1(cfmatknn)
+
+# Calculate performance metrics for neural network model
+neuralnet_accuracy <- sum(diag(cfmatneuralnet)) / sum(cfmatneuralnet)
+neuralnet_sensitivity <- sensitivity(cfmatneuralnet)
+neuralnet_specificity <- specificity(cfmatneuralnet)
+neuralnet_f1 <- F1(cfmatneuralnet)
+
+# Print the performance metrics
+cat("KNN Model:\n")
+cat("Accuracy:", knn_accuracy, "\n")
+cat("Sensitivity:", knn_sensitivity, "\n")
+cat("Specificity:", knn_specificity, "\n")
+cat("F1:", knn_f1, "\n\n")
+
+cat("Neural Network Model:\n")
+cat("Accuracy:", neuralnet_accuracy, "\n")
+cat("Sensitivity:", neuralnet_sensitivity, "\n")
+cat("Specificity:", neuralnet_specificity, "\n")
+cat("F1:", neuralnet_f1, "\n\n")
+
+# Compare the models and identify the best performing model based on each criterion
+best_accuracy_model <- ifelse(knn_accuracy > neuralnet_accuracy, "KNN", "Neural Network")
+best_sensitivity_model <- ifelse(knn_sensitivity > neuralnet_sensitivity, "KNN", "Neural Network")
+best_specificity_model <- ifelse(knn_specificity > neuralnet_specificity, "KNN", "Neural Network")
+best_f1_model <- ifelse(knn_f1 > neuralnet_f1, "KNN", "Neural Network")
+
+cat("Best Model Based on Accuracy:", best_accuracy_model, "\n")
+cat("Best Model Based on Sensitivity:", best_sensitivity_model, "\n")
+cat("Best Model Based on Specificity:", best_specificity_model, "\n")
+cat("Best Model Based on F1:", best_f1_model, "\n")
 
